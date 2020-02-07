c1e87de1e00969534d70b608dd01621cece60e34
Sia819 <jjn970819@gmail.com>
2020-02-07 12:45:18 +0900

    SerializeEX and Example Solutions

---
+++ SerializeEx/SerializeEx/SerializeEx.cs
@@ -0,0 +1,33 @@
+﻿namespace SerializeXML
+{
+    using System.IO;
+    using System.Xml.Serialization;
+
+    public class SerializeEx
+    {
+        public static void Serialization(object obj, string savepath)
+        {
+            // if you use Network driver location, this implementation to solve the permission problems. (backup and remove works)
+            if (File.Exists(savepath))
+                File.Move(savepath, savepath + ".bak");
+            using (StreamWriter wr = new StreamWriter(savepath))
+            {
+                XmlSerializer xs = new XmlSerializer(obj.GetType());
+                xs.Serialize(wr, obj);
+            }
+            if (File.Exists(savepath + ".bak"))
+                File.Delete(savepath + ".bak");
+        }
+
+        public static T DeSerialization<T>(string savepath)
+        {
+            T obj;
+            using (var reader = new StreamReader(savepath))
+            {
+                XmlSerializer xs = new XmlSerializer(typeof(T));
+                obj = (T)xs.Deserialize(reader);
+            }
+            return obj;
+        }
+    }
+}
---
+++ SerializeEx/SerializeExample/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SerializeExample
+{
+    using System;
+    using System.Collections.Generic;
+    using SerializeXML;
+
+    class Program
+    {
+        static void Main()
+        {
+            List<UserInfo> infos = new List<UserInfo>();
+            infos.Add(new UserInfo() { UserName = "John", UserLocation = "US", UserBirth = new DateTime(2000, 01, 01) });
+            infos.Add(new UserInfo() { UserName = "ゆり", UserLocation = "JP", UserBirth = new DateTime(2001, 02, 02) });
+            infos.Add(new UserInfo() { UserName = "철수", UserLocation = "KR", UserBirth = new DateTime(2002, 03, 03) });
+
+            SerializeEx.Serialization(infos, Environment.CurrentDirectory + "\\infos");
+            var loadInfos = SerializeEx.DeSerialization<List<UserInfo>>(Environment.CurrentDirectory + "\\infos");
+            foreach (var i in loadInfos)
+            {
+                string outputResult = "Name : " + i.UserName + "  Location : " + i.UserLocation + " Birth : " + i.UserBirth;
+                Console.WriteLine(outputResult);
+            }
+        }
+    }
+
+    public class UserInfo
+    {
+        public string UserName;
+        public string UserLocation;
+        public DateTime UserBirth;
+    }
+}
